a244aa7e78e11c4e240b6b86604b3a41d03c0aa2
Stefan Karpinski <stefan@karpinski.org>
2014-07-12 21:51:19 -0400

    move the codegen changes from the multithreading branch.

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -419,7 +419,8 @@ void jl_dump_objfile(char *fname, int jit_model)
 #else
         jit_model ? Reloc::PIC_ : Reloc::Default,
 #endif
-        jit_model ? CodeModel::JITDefault : CodeModel::Default,
+        // jit_model ? CodeModel::JITDefault : CodeModel::Default,
+        CodeModel::Default,
         CodeGenOpt::Aggressive // -O3
         ));
 
@@ -4539,6 +4540,8 @@ extern "C" void jl_init_codegen(void)
 #endif
         .setTargetOptions(options)
 #ifdef USE_MCJIT
+        .setCodeModel(CodeModel::Default)
+        .setRelocationModel(Reloc::PIC_)
         .setUseMCJIT(true)
 #endif
     ;
