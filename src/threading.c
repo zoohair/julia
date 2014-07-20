643286ee8a9f5327aebc1ce1650fdc7cdf8993f1
Kiran Pamnany <kpamnany@gmail.com>
2014-07-19 20:27:53 -0400

    Required headers.

--- src/threading.c
+++ src/threading.c
@@ -1,4 +1,31 @@
 /*
+Copyright (c) 2014, Intel Corporation
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Intel Corporation nor the names of its contributors
+      may be used to endorse or promote products derived from this software
+      without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*
   threading infrastructure
   . thread and threadgroup creation
   . thread function
@@ -75,7 +102,7 @@ int ti_threadcreate(uint64_t *pthread_id, int proc_num,
     CPU_SET(proc_num, &cset);
 
     pthread_attr_init(&attr);
-    //pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cset);
+    pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cset);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
     return pthread_create(pthread_id, &attr, thread_fun, thread_arg);
 }
@@ -88,7 +115,7 @@ void ti_threadsetaffinity(uint64_t pthread_id, int proc_num)
 
     CPU_ZERO(&cset);
     CPU_SET(proc_num, &cset);
-    //pthread_setaffinity_np(pthread_id, sizeof(cpu_set_t), &cset);
+    pthread_setaffinity_np(pthread_id, sizeof(cpu_set_t), &cset);
 }
 
 
